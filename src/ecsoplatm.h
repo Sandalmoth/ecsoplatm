151b8c7bc27c38ab633ab0fee4cb80920ee5d798
Jonathan <sandalmoth@pm.me>
2022-01-02 11:09:29 +0100

    consolidate code into one source file, since the building blocks are far too incommple to be used on their own anyway

--- src/ecsoplatm.h
+++ src/ecsoplatm.h
@@ -1,12 +1,22 @@
 #pragma once
 
-
+#include <algorithm>
 #include <array>
+#include <atomic>
+#include <cstdint>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <mutex>
 #include <string>
+#include <thread>
+#include <tuple>
+#include <type_traits>
 #include <vector>
 
-#include "flowpool.h"
-#include "interval_map.h"
+
+namespace ecs { class Flowpool; }
+inline std::ostream &operator<<(std::ostream &, ecs::Flowpool &);
 
 
 namespace ecs {
@@ -14,6 +24,235 @@ namespace ecs {
   const int BLOCK_SIZE = 256;
   const int CACHE_BITS = 4;
   const int CACHE_SIZE = 0x1 << CACHE_BITS;
+
+
+  template <typename T>
+  struct IntervalMap {
+
+    /*
+     * This is a very simple struct for storing non-overlapping intervals
+     * So we can store a T in some interval [4, 5] = t1
+     * And then store another T in like [2, 4] = t2
+     * Which creates [2, 4] = t2, [5] = t1
+     * And obviously we can do lookups with an interavl as well
+     */
+
+    void set(int first, int last, T value) {
+      // first put our key into the vector
+      auto it = std::lower_bound(
+          data.begin(), data.end(), first,
+          [](const std::tuple<int, int, T> &a, const int &b) {
+            return std::get<0>(a) < b;
+          });
+      it = data.insert(it, std::make_tuple(first, last, value));
+      // then, determine if we need to shorten the previous interval
+      auto prev = it;
+      while (prev != data.begin()) {
+        --prev;
+        if (std::get<1>(*prev) > first) {
+          if (std::get<1>(*prev) > last) {
+            // if we're inserting in the middle, we need to create a new entry
+            int tmp = std::get<1>(*prev);
+            std::get<1>(*prev) = first;
+            ++it;
+            data.insert(it, std::make_tuple(last, tmp, std::get<2>(*prev)));
+            return;
+          } else {
+            // otherwise we just shorten the prior
+            std::get<1>(*prev) = first;
+          }
+        } else {
+          break;
+        }
+      }
+      // then, determine the number of following intervals that were overwritten
+      auto next = it;
+      ++next;
+      while (next != data.end()) {
+        if (std::get<1>(*next) <= last) {
+          // we've entirely overwritten this one
+          next = data.erase(next);
+          --next;
+        } else if (std::get<0>(*next) < last) {
+          std::get<0>(*next) = last;
+        }
+        ++next;
+      }
+    }
+
+    std::vector<T> get(int first, int last) {
+      std::vector<T> result;
+      for (auto &[f, l, v]: data) {
+        if (!(last <= f) & !(first >= l)) {
+          result.push_back(v);
+        }
+      }
+      return result;
+    }
+
+    std::vector<std::tuple<int, int, T>> data;
+  };
+
+
+  class Flowpool {
+
+    /*
+    * This thread pool is essentially a rewrite and modified version of
+    * SandSnip3r's fork ( https://github.com/SandSnip3r/thread-pool ) of
+    * Barak Shashany's thread_pool.hpp library (
+    * https://github.com/bshoshany/thread-pool ).
+    *
+    * It also implements a waiting system where a pushed task can be required
+    * to wait for some prior task to finish before entering the work queue.
+    * This enables us to queue tasks that will sequentially modify the same data.
+    */
+
+  public:
+
+    enum TaskStatus {
+      WAITING,
+      IN_PROGRESS,
+      DONE,
+      NUM_TASK_STATUS
+    };
+
+    Flowpool() {
+      n_threads = std::thread::hardware_concurrency();
+      create_threads();
+    }
+
+    Flowpool(int n_threads_)
+      : n_threads(n_threads_) {
+      create_threads();
+    }
+
+    ~Flowpool()
+    {
+      wait_for_tasks();
+      running = false;
+      destroy_threads();
+    }
+
+
+    void wait_for_tasks() {
+      std::unique_lock<std::mutex> lock(tasks_mutex);
+      tasks_done_condition.wait(lock, [&] {
+        return (n_tasks == 0);
+      });
+
+      total_tasks = 0;
+      tasks.clear();
+      flags.clear();
+      conditions.clear();
+    }
+
+    template <typename F>
+    int push_task(const F &task,
+                  std::vector<int> conds) {
+      int id;
+      {
+        std::scoped_lock lock(tasks_mutex);
+        tasks.push_back(task);
+        conditions.push_back(std::move(conds));
+        flags.push_back(WAITING);
+        ++n_tasks;
+        id = total_tasks++;
+      }
+      task_available_condition.notify_one();
+      return id;
+    }
+
+    template <typename F,
+              std::convertible_to<int>... C>
+    int push_task(const F &task, C... conds) {
+      std::vector<int> vconds {conds...};
+      return push_task(task, vconds);
+    }
+
+
+  private:
+
+    void create_threads() {
+      threads = std::make_unique<std::thread[]>(n_threads);
+      for (int i = 0; i < n_threads; ++i) {
+        threads[i] = std::thread(&Flowpool::worker, this);
+      }
+    }
+
+    void destroy_threads()
+    {
+      task_available_condition.notify_all();
+      for (int i = 0; i < n_threads; i++)
+        {
+          threads[i].join();
+        }
+    }
+
+
+    void worker() {
+      while (running) {
+
+        std::unique_lock<std::mutex> lock(tasks_mutex);
+
+        task_available_condition.wait(lock, [&]{
+          return !tasks.empty() || !running;
+        });
+
+        if (running) {
+          // grab the first task from tasks that is doable
+          // a task is doable if conds[task].size == 0
+          // or if all flags[conds[task]] == true
+          // possible speedup: don't start from 0
+          int task_id = 0;
+          while (task_id < total_tasks) {
+            if (flags[task_id] == WAITING) {
+              // it's a waiting task, see if it can run
+              bool doable = true;
+              for (auto cond: conditions[task_id]) {
+                // possible speedup: short circuit loop
+                doable = doable && (flags[cond] == DONE);
+              }
+              if (doable) {
+                auto task = std::move(tasks[task_id]);
+                flags[task_id] = IN_PROGRESS;
+
+                lock.unlock();
+                task();
+                lock.lock();
+
+                flags[task_id] = DONE;
+                --n_tasks;
+                break;
+              }
+            }
+            ++task_id;
+          }
+
+          lock.unlock();
+          tasks_done_condition.notify_one();
+        }
+      }
+    }
+
+    friend std::ostream &::operator<<(std::ostream &, ecs::Flowpool &);
+
+    int n_threads;
+    std::unique_ptr<std::thread[]> threads;
+
+    std::mutex tasks_mutex; // locks tasks, n_tasks, flags, and conditions
+
+    std::condition_variable task_available_condition;
+    std::condition_variable tasks_done_condition;
+
+    std::atomic<bool> running {true};
+    int n_tasks {0}; // total number of waiting, queued, and running tasks
+    int total_tasks {0}; // total number of tasks queued since last wait
+
+    std::vector<char> flags;
+    std::vector<std::function<void()>> tasks;
+    std::vector<std::vector<int>> conditions; // indices into flags
+  };
+
 
   struct ComponentInterface {
     virtual void update() = 0;
@@ -418,9 +657,44 @@ namespace ecs {
 
   };
 
-
 } // end namespace ecs
 
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &out, ecs::IntervalMap<T> &im) {
+  out << '[';
+  for (auto &[first, last, value] : im.data) {
+    out << '(' << first << ' ' << value << ' ' << last << ')';
+  }
+  out << ']';
+  return out;
+}
+
+inline std::ostream &operator<<(std::ostream &out, ecs::Flowpool &pool) {
+  std::scoped_lock lock(pool.tasks_mutex);
+  std::cout << pool.n_tasks << " unfinished out of " << pool.total_tasks
+            << " total" << std::endl;
+  for (int i = 0; i < pool.total_tasks; ++i) {
+    std::string flag;
+    switch (pool.flags[i]) {
+    case ecs::Flowpool::WAITING:
+      flag = "waiting";
+      break;
+    case ecs::Flowpool::IN_PROGRESS:
+      flag = "in_progress";
+      break;
+    case ecs::Flowpool::DONE:
+      flag = "done";
+      break;
+    }
+    std::cout << '(' << i << ' ' << flag;
+    for (auto &w : pool.conditions[i]) {
+      std::cout << ' ' << w;
+    }
+    std::cout << ')' << std::endl;
+  }
+  return out;
+}
 
 template <typename T>
 inline std::ostream &operator<<(std::ostream &out, ecs::Component<T> &c) {
